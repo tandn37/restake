6e1c1c577941166b4d004ab4e99c5944964c36b2
Thrull <dntan90@gmail.com>
2022-07-21 17:19:07 +0700

    Revert "update gas limit"
    This reverts commit 05bd43a7ef1b1ae2abbdf48b160e30da3a4653bc.

--- scripts/EthSigning.mjs
+++ scripts/EthSigning.mjs
@@ -65,7 +65,6 @@ const buildTxBytes = ({ SigningClient, ethSigner, chainId, network }) => async (
 }
 
 const estimateGas = (signingClient) => async (messages, pubkey, sequence) => {
-  return 7000000;
   const anyMsgs = messages.map((m) => signingClient.registry.encodeAsAny(m));
   const encodedPubkey = encodeSecp256k1Pubkey(pubkey);
   const result = await signingClient.forceGetQueryClient().tx.simulate(anyMsgs, '', encodedPubkey, sequence);
@@ -81,7 +80,6 @@ const signAndBroadcast = async (client, messages, memo) => {
     const pubkey = accounts[0].pubkey;
     const gasLimit = await estimateGas(StargateSigningClient)(messages, pubkey, sequence);
     timeStamp('GAS_PRICE', client.network.gasPrice);
-    timeStamp('GAS_LIMIT', gasLimit);
     const txBytes = await buildTxBytes({
       SigningClient: client.signingClient,
       ethSigner: client.ethSigner,
